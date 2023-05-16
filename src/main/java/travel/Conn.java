cf6daf36489223c2cf4cc5db5469519baaf52fc9
Hasanov23 <zamilhh@code.edu.az>
2023-05-16 12:56:33 +0200

    Initial commit

---
+++ src/main/java/travel/Conn.java
@@ -0,0 +1,20 @@
+package travel;
+
+import java.sql.*;
+
+public class Conn {
+    Connection c;
+
+    Statement s;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tms", "root", "swehasanov");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
